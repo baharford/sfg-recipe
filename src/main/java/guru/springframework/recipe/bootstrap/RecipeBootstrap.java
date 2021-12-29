681975653b99910f220ebf92864554719246466a
m02242a <baharford@gmail.com>
2021-12-29 09:14:45 -0500

    updated code to use setters for jpa bi-directional relationships

--- src/main/java/guru/springframework/recipe/bootstrap/RecipeBootstrap.java
+++ src/main/java/guru/springframework/recipe/bootstrap/RecipeBootstrap.java
@@ -108,12 +108,18 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
 		guacRecipe.setPrepTime(10);
 		guacRecipe.setCookTime(0);
 		guacRecipe.setDifficulty(Difficulty.EASY);
-		guacRecipe.setDirections("1 Cut avado, remove flesh: Cut the avacados in half.  Remove seed.  Score the inside of the avacodo with blunt knife and scoop out the inside." +
-								"\n" + 
-								"2 Mash with a ford: Using a fork, roughtly mash the avocado" + 
-								"\n" + 
-								"3 Add salt, lime juice, and the rest");
-		
+        guacRecipe.setDirections("1 Cut avocado, remove flesh: Cut the avocados in half. Remove seed. Score the inside of the avocado with a blunt knife and scoop out the flesh with a spoon" +
+                "\n" +
+                "2 Mash with a fork: Using a fork, roughly mash the avocado. (Don't overdo it! The guacamole should be a little chunky.)" +
+                "\n" +
+                "3 Add salt, lime juice, and the rest: Sprinkle with salt and lime (or lemon) juice. The acid in the lime juice will provide some balance to the richness of the avocado and will help delay the avocados from turning brown.\n" +
+                "Add the chopped onion, cilantro, black pepper, and chiles. Chili peppers vary individually in their hotness. So, start with a half of one chili pepper and add to the guacamole to your desired degree of hotness.\n" +
+                "Remember that much of this is done to taste because of the variability in the fresh ingredients. Start with this recipe and adjust to your taste.\n" +
+                "4 Cover with plastic and chill to store: Place plastic wrap on the surface of the guacamole cover it and to prevent air reaching it. (The oxygen in the air causes oxidation which will turn the guacamole brown.) Refrigerate until ready to serve.\n" +
+                "Chilling tomatoes hurts their flavor, so if you want to add chopped tomato to your guacamole, add it just before serving.\n" +
+                "\n" +
+                "\n" +
+                "Read more: http://www.simplyrecipes.com/recipes/perfect_guacamole/#ixzz4jvpiV9Sd");		
 	
 		Notes guacNotes = new Notes();
 		guacNotes.setRecipeNotes("For a very quick guacamole just take a 1/4 cup of salsa and mix it in with your mashed avocados.\n" + 
@@ -122,18 +128,19 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
 		guacNotes.setRecipe(guacRecipe);
 		guacRecipe.setNotes(guacNotes);
 		
-		guacRecipe.getIngredients().add(new Ingredient("ripe avocados", new BigDecimal(2), eachUom, guacRecipe));
-		guacRecipe.getIngredients().add(new Ingredient("kosher salt", new BigDecimal(.5), teaspoonUom, guacRecipe));
-		guacRecipe.getIngredients().add(new Ingredient("fresh lime juice or lemon juice", new BigDecimal(2), tablespoonUom, guacRecipe));
-		guacRecipe.getIngredients().add(new Ingredient("minced red onion", new BigDecimal(2), tablespoonUom, guacRecipe));
-		guacRecipe.getIngredients().add(new Ingredient("serrano chiles", new BigDecimal(2), eachUom, guacRecipe));
-		guacRecipe.getIngredients().add(new Ingredient("cilantro", new BigDecimal(2), dashUom, guacRecipe));
+        guacRecipe.addIngredient(new Ingredient("ripe avocados", new BigDecimal(2), eachUom));
+        guacRecipe.addIngredient(new Ingredient("Kosher salt", new BigDecimal(".5"), teaspoonUom));
+        guacRecipe.addIngredient(new Ingredient("fresh lime juice or lemon juice", new BigDecimal(2), tablespoonUom));
+        guacRecipe.addIngredient(new Ingredient("minced red onion or thinly sliced green onion", new BigDecimal(2), tablespoonUom));
+        guacRecipe.addIngredient(new Ingredient("serrano chiles, stems and seeds removed, minced", new BigDecimal(2), eachUom));
+        guacRecipe.addIngredient(new Ingredient("Cilantro", new BigDecimal(2), tablespoonUom));
+        guacRecipe.addIngredient(new Ingredient("freshly grated black pepper", new BigDecimal(2), dashUom));
+        guacRecipe.addIngredient(new Ingredient("ripe tomato, seeds and pulp removed, chopped", new BigDecimal(".5"), eachUom));
 		
 		guacRecipe.getCategories().add(americanCategory);
 		guacRecipe.getCategories().add(mexicanCategory);
 		
 		recipes.add(guacRecipe);
-		
 		
 		// Yummy Tacos
 		Recipe tacoRecipe = new Recipe();
@@ -141,23 +148,42 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
 		tacoRecipe.setPrepTime(9);
 		tacoRecipe.setCookTime(20);
 		tacoRecipe.setDifficulty(Difficulty.MODERATE);
-		tacoRecipe.setDirections("1 Prepare gas or charcoal grill for midium-high, direct heat" +
-								"\n" + 
-								"2 Make the marinade and coat the chicken" + 
-								"\n" + 
-								"3 Grill the chicken, yada, yada, yada");
-
+        tacoRecipe.setDirections("1 Prepare a gas or charcoal grill for medium-high, direct heat.\n" +
+                "2 Make the marinade and coat the chicken: In a large bowl, stir together the chili powder, oregano, cumin, sugar, salt, garlic and orange zest. Stir in the orange juice and olive oil to make a loose paste. Add the chicken to the bowl and toss to coat all over.\n" +
+                "Set aside to marinate while the grill heats and you prepare the rest of the toppings.\n" +
+                "\n" +
+                "\n" +
+                "3 Grill the chicken: Grill the chicken for 3 to 4 minutes per side, or until a thermometer inserted into the thickest part of the meat registers 165F. Transfer to a plate and rest for 5 minutes.\n" +
+                "4 Warm the tortillas: Place each tortilla on the grill or on a hot, dry skillet over medium-high heat. As soon as you see pockets of the air start to puff up in the tortilla, turn it with tongs and heat for a few seconds on the other side.\n" +
+                "Wrap warmed tortillas in a tea towel to keep them warm until serving.\n" +
+                "5 Assemble the tacos: Slice the chicken into strips. On each tortilla, place a small handful of arugula. Top with chicken slices, sliced avocado, radishes, tomatoes, and onion slices. Drizzle with the thinned sour cream. Serve with lime wedges.\n" +
+                "\n" +
+                "\n" +
+                "Read more: http://www.simplyrecipes.com/recipes/spicy_grilled_chicken_tacos/#ixzz4jvtrAnNm");
 		Notes tacoNotes = new Notes();
 		tacoNotes.setRecipeNotes("These are notes about the tacos...");
 		tacoNotes.setRecipe(tacoRecipe);
 		tacoRecipe.setNotes(tacoNotes);
 		
-		
-		tacoRecipe.getIngredients().add(new Ingredient("Ancho Chili Powder", new BigDecimal(2), pintUom, tacoRecipe));
-		tacoRecipe.getIngredients().add(new Ingredient("Dried Oregano", new BigDecimal(1), cupUom, tacoRecipe));
-		tacoRecipe.getIngredients().add(new Ingredient("Dried Cumin", new BigDecimal(1), teaspoonUom, tacoRecipe));
-		tacoRecipe.getIngredients().add(new Ingredient("Boneless Chicken Thighs", new BigDecimal(4), eachUom, tacoRecipe));
-		tacoRecipe.getIngredients().add(new Ingredient("Small Corn Tortillas", new BigDecimal(8), eachUom, tacoRecipe));
+        tacoRecipe.addIngredient(new Ingredient("Ancho Chili Powder", new BigDecimal(2), tablespoonUom));
+        tacoRecipe.addIngredient(new Ingredient("Dried Oregano", new BigDecimal(1), teaspoonUom));
+        tacoRecipe.addIngredient(new Ingredient("Dried Cumin", new BigDecimal(1), teaspoonUom));
+        tacoRecipe.addIngredient(new Ingredient("Sugar", new BigDecimal(1), teaspoonUom));
+        tacoRecipe.addIngredient(new Ingredient("Salt", new BigDecimal(".5"), teaspoonUom));
+        tacoRecipe.addIngredient(new Ingredient("Clove of Garlic, Choppedr", new BigDecimal(1), eachUom));
+        tacoRecipe.addIngredient(new Ingredient("finely grated orange zestr", new BigDecimal(1), tablespoonUom));
+        tacoRecipe.addIngredient(new Ingredient("fresh-squeezed orange juice", new BigDecimal(3), tablespoonUom));
+        tacoRecipe.addIngredient(new Ingredient("Olive Oil", new BigDecimal(2), tablespoonUom));
+        tacoRecipe.addIngredient(new Ingredient("boneless chicken thighs", new BigDecimal(4), tablespoonUom));
+        tacoRecipe.addIngredient(new Ingredient("small corn tortillasr", new BigDecimal(8), eachUom));
+        tacoRecipe.addIngredient(new Ingredient("packed baby arugula", new BigDecimal(3), cupUom));
+        tacoRecipe.addIngredient(new Ingredient("medium ripe avocados, slic", new BigDecimal(2), eachUom));
+        tacoRecipe.addIngredient(new Ingredient("radishes, thinly sliced", new BigDecimal(4), eachUom));
+        tacoRecipe.addIngredient(new Ingredient("cherry tomatoes, halved", new BigDecimal(".5"), pintUom));
+        tacoRecipe.addIngredient(new Ingredient("red onion, thinly sliced", new BigDecimal(".25"), eachUom));
+        tacoRecipe.addIngredient(new Ingredient("Roughly chopped cilantro", new BigDecimal(4), eachUom));
+        tacoRecipe.addIngredient(new Ingredient("cup sour cream thinned with 1/4 cup milk", new BigDecimal(4), cupUom));
+        tacoRecipe.addIngredient(new Ingredient("lime, cut into wedges", new BigDecimal(4), eachUom));
 
 		tacoRecipe.getCategories().add(mexicanCategory);
 		tacoRecipe.getCategories().add(americanCategory);
@@ -167,8 +193,5 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
 		return recipes;
 		
 	}
-
-
-	
 	
 }
