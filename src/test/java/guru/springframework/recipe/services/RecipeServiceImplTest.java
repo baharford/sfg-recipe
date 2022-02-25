aa1884ff515a24bcbdf92e2341131b3f22863e6e
m02242a <baharford@gmail.com>
2022-02-25 09:26:43 -0500

    added code to delete a recipe

--- src/test/java/guru/springframework/recipe/services/RecipeServiceImplTest.java
+++ src/test/java/guru/springframework/recipe/services/RecipeServiceImplTest.java
@@ -80,4 +80,19 @@ class RecipeServiceImplTest {
 		Mockito.verify(recipeRepository, times(1)).findAll();
 	}
 
+	@Test
+	void testDeleteById() throws Exception {
+
+		//given
+		Long idToDelete = 2L;
+
+		//when
+		recipeService.deleteById(idToDelete);
+
+		//no 'when', since method has void return type
+
+		//then
+		verify(recipeRepository, times(1)).deleteById(anyLong());
+	}
+
 }
