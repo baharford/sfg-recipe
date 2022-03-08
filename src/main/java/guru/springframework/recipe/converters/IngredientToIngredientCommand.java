5483384ea83cf86587ca9e52435577edd977d6e7
m02242a <baharford@gmail.com>
2022-03-08 09:44:58 -0500

    Fixed IngredientToIngredientCommand to check for ingredient.getRecipe() != null.  This fixed the failing tests.

--- src/main/java/guru/springframework/recipe/converters/IngredientToIngredientCommand.java
+++ src/main/java/guru/springframework/recipe/converters/IngredientToIngredientCommand.java
@@ -24,13 +24,11 @@ public class IngredientToIngredientCommand implements Converter<Ingredient, Ingr
     @Nullable
     @Override
     public IngredientCommand convert(Ingredient ingredient) {
-        if (ingredient == null) {
-            return null;
-        }
-
         IngredientCommand ingredientCommand = new IngredientCommand();
         ingredientCommand.setId(ingredient.getId());
-        ingredientCommand.setRecipeId((ingredient.getRecipe().getId()));
+        if (ingredient.getRecipe() != null) {
+            ingredientCommand.setRecipeId(ingredient.getRecipe().getId());
+        }
         ingredientCommand.setAmount(ingredient.getAmount());
         ingredientCommand.setDescription(ingredient.getDescription());
         ingredientCommand.setUom(uomConverter.convert(ingredient.getUom()));
